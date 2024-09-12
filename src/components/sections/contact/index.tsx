6109fcae779fab841b0a7743fc39432895cc0190
jhukal9@gmail.com <jhukal9@gmail.com>
2024-09-12 22:27:32 +0545

    updated the contact layout

--- src/components/sections/contact/index.tsx
+++ src/components/sections/contact/index.tsx
@@ -1,22 +1,60 @@
+"use client";
+import React, { useEffect } from "react";
 import GithubIcon from "@/Icons/Github";
 import LinkedInIcon from "@/Icons/LinkedIn";
 import Link from "next/link";
+import UpworkIcon from "@/Icons/upwork";
 
 import "./contact.scss";
-import PhoneIcon from "@/Icons/Phone";
-import MailIcon from "@/Icons/Mail";
-import LocationIcon from "@/Icons/Location";
-import UpworkIcon from "@/Icons/upwork";
+
 export default function Contact() {
+  const contactRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          contactRef.current?.classList.add("active");
+        } else {
+          contactRef.current?.classList.remove("active");
+        }
+      });
+    }, { threshold: 0.9 });
+
+    if (contactRef.current) {
+      observer.observe(contactRef.current);
+    }
+
+    return () => {
+      if (contactRef.current) {
+        observer.unobserve(contactRef.current);
+      }
+    };
+  }, []);
+
   return (
     <section className="contact py-[140px]" id="contact">
       <div className="container">
-        {/* <div className="lg:grid grid-cols-2 lg:gap-12">
-          <div className="col-span-1 mb-8">
-            <h2 className="mb-8">Let&rsquo;s Connect</h2>
-            <p className="text-[18px] mb-8">If you ever want to make some small talk you can find me on social media </p>
-            <div className="flex justify-start items-center gap-6 social-links">
-              <Link href={"https://github.com/Arjun-Jhukal"} className="hover:scale-105 transition-transform duration-200 ease-in" target="_blank">
+        <div className="section-title text-center mb-[60px] max-w-[1240px]">
+          <h2>Let's Connect</h2>
+          <p>
+            Whether you're looking to collaborate on a project, seek advice, or just want to connect, feel free to reach out!
+          </p>
+        </div>
+        <div className="contact-card" ref={contactRef}>
+          <div className="front-face">
+            <div className="ff-content">
+              <h3>Arjun Jhukal</h3>
+              <p>Frontend Developer</p>
+            </div>
+          </div>
+          <div className="back-face flex-col">
+            <div className="flex justify-start items-center gap-6 social-links mb-[24px]">
+              <Link
+                href={"https://github.com/Arjun-Jhukal"}
+                className="hover:scale-105 transition-transform duration-200 ease-in"
+                target="_blank"
+              >
                 <GithubIcon />
               </Link>
               <Link
@@ -34,54 +72,6 @@ export default function Contact() {
                 <UpworkIcon />
               </Link>
             </div>
-          </div>
-
-          <div className="col-span-1">
-            <div className="contact__item">
-              <div className="icon">
-                <PhoneIcon />
-              </div>
-              <div className="text">
-                <Link href={"tel:+977 9812767046"} className="text-[22px] text-black-300 block">
-                  +977 9812767046
-                </Link>
-                <Link href={"tel:+977 9848413085"} className="text-[22px] text-black-300 block">
-                  +977 9848413085
-                </Link>
-              </div>
-            </div>
-            <div className="contact__item">
-              <div className="icon">
-                <MailIcon />
-              </div>
-              <div className="text">
-                <Link href={"mailto:connectjhukal@gmail.com"} className="text-[22px] text-black-300 block">
-                  connectjhukal@gmail.com
-                </Link>
-              </div>
-            </div>
-            <div className="contact__item">
-              <div className="icon">
-                <LocationIcon />
-              </div>
-              <div className="text">
-                <p className="text-[18px]">Kathmandu, Nepal</p>
-              </div>
-            </div>
-          </div>
-        </div> */}
-        <div className="section-title text-center mb-[60px]">
-          <h2>Let's connect</h2>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, doloremque!</p>
-        </div>
-        <div className="contact-card">
-          <div className="front-face">
-            <div className="ff-content">
-              <h3>Arjun Jhukal</h3>
-              <p>Frontend Developer</p>
-            </div>
-          </div>
-          <div className="back-face">
             <ul>
               <li>
                 <Link href={"tel:+977 9812767046"}>PH. +977 9812767046</Link>
@@ -90,7 +80,7 @@ export default function Contact() {
                 <Link href={"mailto:connectjhukal@gmail.com"}>E-Mail. connectjhukal@gmail.com</Link>
               </li>
               <li>
-                <Link href={"https://arjunjhukal.com.np"}>https://arjunjhukal.com.np</Link>
+                <Link href={"https://arjunjhukal.com.np"}>Website. https://arjunjhukal.com.np</Link>
               </li>
             </ul>
           </div>
